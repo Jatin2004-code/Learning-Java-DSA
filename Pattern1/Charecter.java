4eb4d41ce6509890443330e42fc73ac847a25866
Jatin Kanojiya <jatinkanoujiya8@gmail.com>
2025-08-03 14:53:31 +0530

    Added Pattern1 code

---
+++ Pattern1/Charecter.java
@@ -0,0 +1,16 @@
+package Pattern1;
+
+public class Charecter {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int n = 7;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+    
+}
---
+++ Pattern1/NestedLoopStar.java
@@ -0,0 +1,14 @@
+package Pattern1;
+
+public class NestedLoopStar {
+    public static void main(String[] args) {
+        for(int line=1; line<=10; line++){
+            //one loop
+            for(int star=1; star<=line; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
