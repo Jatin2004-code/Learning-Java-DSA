610705b64d34945b553418a515f0341e1d88cb94
Jatin Kanojiya <jatinkanoujiya8@gmail.com>
2025-08-04 15:50:37 +0530

    feat: Add Product A or B Code

---
+++ Function/ProductAorB.java
@@ -0,0 +1,19 @@
+package Function;
+
+public class ProductAorB {
+        public static int multiply(int a, int b){ // parameters or formal Prameters
+        int product = a*b ;
+         return  product;
+  }
+    
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5; 
+        int prod = multiply (a,b);// gument or actual parameters
+       System.out.println(" a*b ="+prod);
+
+        
+    }
+    
+}
+
