839574b89402c5587320fe90994e0abb4bc676ed
Jatin Kanojiya <jatinkanoujiya8@gmail.com>
2025-08-07 23:50:14 +0530

    feat: Add ZeroAndOneTraingle Code

---
+++ Pattern2/ZeroAndOneTraingle.java
@@ -0,0 +1,12 @@
+package Pattern2;
+
+public class ZeroAndOneTraingle {
+    public static void main(String[] args) {
+
+    System.out.println("1");
+    System.out.println("01");
+    System.out.println("101");
+    System.out.println("0101");
+    System.out.println("10101");
+}
+}
