ffa332253aabc45491902f62cd811a8752de7766
Jatin Kanojiya <jatinkanoujiya8@gmail.com>
2025-08-10 10:56:08 +0530

    feat: Add Basic Array  Code

---
+++ Array/BasicArray.java
@@ -0,0 +1,16 @@
+//for a output - javac ArrayCC.java
+
+package Array;
+
+public class BasicArray {
+    public static void main(String[] args) {
+        int marks [] = new int[50];
+
+        int number [] = {1,2,3};
+        int morenumber [] = {4,5,6};
+        String fruits [] = {"Apple","mango","orange"};
+
+
+    }
+    
+}
