4d25f6f4c7fcc17afe682d899b018175ba0dfd0a
Jatin Kanojiya <jatinkanoujiya8@gmail.com>
2025-08-18 20:55:24 +0530

    Add String code

---
+++ String/CharAt.java
@@ -0,0 +1,20 @@
+package String;
+
+public class CharAt {
+    public static void printlatters(String str) {
+        for(int i=0; i<str.length(); i++){
+        }
+            System.out.println();
+       
+    }
+    public static void main(String[] args) {
+        String firstname = "jatin";
+        String lastname = "Kanojiya";
+        String fullname = firstname + " " + lastname; 
+        
+        printlatters(fullname);
+    
+    }
+    
+
+}
