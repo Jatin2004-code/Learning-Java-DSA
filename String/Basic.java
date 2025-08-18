442422214ce184e227983a7e5b9be94e6220b02f
Jatin Kanojiya <jatinkanoujiya8@gmail.com>
2025-08-18 20:54:45 +0530

    Add String code

---
+++ String/Basic.java
@@ -0,0 +1,19 @@
+package String;
+import java.util.*;
+
+public class Basic{
+    public static void main(String[] args) {
+        char arr [] = {'a' , 'b', 'c' , 'd'};
+        String str ="abcd";
+        String str2 = new String("xyz");
+         
+        //string are IMUTABLE 
+        Scanner sc = new Scanner(System.in);
+        String name ;
+       name = sc.nextLine();
+      
+       System.err.println(name);
+
+    }
+    
+}
