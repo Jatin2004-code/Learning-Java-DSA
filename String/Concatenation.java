cbf3bb84050d977fcf35284b22b5efe61be8bcc4
Jatin Kanojiya <jatinkanoujiya8@gmail.com>
2025-08-19 14:57:05 +0530

    Add String code

---
+++ String/Concatenation.java
@@ -0,0 +1,13 @@
+package String;
+
+public class Concatenation {
+    public static void main(String[] args) {
+        String firstname = "jatin";
+        String lastname = "Kanojiya";
+        String fullname = firstname + " " + lastname; 
+        System.out.println(fullname);
+  
+    
+    }
+    
+}
