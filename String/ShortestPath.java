a2f0493412e92e9d55eec24cfce122152f1e4bc3
Jatin Kanojiya <jatinkanoujiya8@gmail.com>
2025-08-20 12:36:53 +0530

    Add String code

---
+++ String/ShortestPath.java
@@ -0,0 +1,52 @@
+package String;
+
+import Statements.elseif;
+
+public class ShortestPath {
+    public static float getshortestpath(String path) {
+ int x=0 , y=0 ;
+ 
+ for(int i=0; i<path.length(); i++){
+  char dir = path.charAt(i);
+
+//     DIRECTION:
+
+//               N
+//               . 
+//          W .  .  . E
+//               .
+//               S
+
+
+
+
+  //south
+  if(dir == 's'){
+    y--;
+
+  }
+  //north 
+  else if (dir == 'N'){
+    y++;
+
+  }
+  //west
+  else if(dir == 'w'){
+    x--;
+
+  }
+  //East
+  else {
+    x++;
+  }
+    
+}
+ int x2 = x*x;
+ int y2 = y*y;
+ return (float)Math.sqrt(x2 + y2);
+}
+public static void main(String[] args) {
+    String path = "WNEENESENNN";
+    System.out.println(getshortestpath(path));
+}
+}
