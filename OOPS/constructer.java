a00d17253cfbb9e9aa32a4ba60f1b832a0b99c5e
Jatin Kanojiya <jatinkanoujiya8@gmail.com>
2025-09-11 22:22:04 +0530

    Add  OOPS/abstract.java

---
+++ OOPS/constructer.java
@@ -0,0 +1,35 @@
+package OOPS;
+
+public class constructer {
+    
+    public static void main(String[] args) {
+     Student s1 = new Student();
+     Student s2 = new Student("jatin");
+     Student s3 = new Student(123);
+    }
+}
+ class Student {
+    String name ;
+    int roll;
+
+
+    Student () {
+        System.out.println("constructor is called ....");
+    }                                                                                                                                                                                                                                           
+    Student(String name){
+        this.name = name;
+ }
+ Student (int roll){
+    this.roll = roll;
+}
+}
+
+
+
+
+
+
+
+
+
+ 
