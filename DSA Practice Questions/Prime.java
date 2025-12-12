38caccff646ee622a74c9d5f59690a206f2a965b
Jatin Kanojiya <jatinkanoujiya8@gmail.com>
2025-12-12 23:30:12 +0530

    Add code

---
+++ DSA Practice Questions/Prime.java
@@ -0,0 +1,28 @@
+public class Prime {
+
+    // Function to check prime number
+    public static boolean isPrime(int n) {
+
+        if (n <= 1) {
+            return false;      // 1, 0, negative → Prime nahi hote
+        }
+
+        boolean isPrime = true;
+
+        for(int i = 2; i <= n - 1; i++){
+            if(n % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+
+        return isPrime;
+    }
+
+
+    public static void main(String args[]){
+        System.out.println(isPrime(5));   // true
+        System.out.println(isPrime(12));  // false
+        System.out.println(isPrime(17));  // true
+    }
+}
