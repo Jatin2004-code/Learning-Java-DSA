678252b676175a26f3e07af1db94d64605684d42
Jatin Kanojiya <jatinkanoujiya8@gmail.com>
2026-01-02 10:25:32 +0530

    Add java code 5

---
+++ LinkedList/PrintLL.java
@@ -0,0 +1,33 @@
+package LinkedList;
+public class PrintLL {
+
+    Node head;
+    Node tail;
+    int size;
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Print / display Linked List
+    public void printLL() {
+        if (head == null) {
+            System.out.println("LL is empty");
+            return;
+        }
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+}
